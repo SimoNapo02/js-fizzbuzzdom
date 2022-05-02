3bb0bc5a7a103ac0bc88faa3f50834c6aef3c2dc
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-05-02 20:53:47 +0200

    js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@ const ul = document.querySelector('.list');
 const li = document.createElement('li');
 li.append(i);
 ul.append(li);
+//°Stampo "Fizz" per i multipli di 3
+//°Stampo "Buzz" per i multipli di 5
+//°Stampo "FizzBuzz" per i multipli di entrambi i numeri
+if ((i % 3 === 0) && (i % 5 === 0)){
+    console.log ('FizzBuzz')
+    li.innerText = "FizzBuzz"
+    li.classList.add("fizzbuzz");
+
+} else if (i % 3 === 0){
+    console.log ('Fizz')
+    li.innerText = "Fizz"
+    li.classList.add("fizz");
+} else if (i % 5 === 0) {
+    console.log ('Buzz')
+    li.innerText = "Buzz"
+    li.classList.add("buzz");
+} else {
+    console.log (i)
+    li.classList.add("default");
+};
+
+};
